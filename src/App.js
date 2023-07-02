bc242bb25ebf9b57214f6119283a035b8861f32b
Heechem <hichempeter@gmail.com>
2023-07-02 09:00:18 -0600

    Adding MOC Json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Header from "./Header";
 import Main from "./Main";
 
 const App = () => {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="app">
       <Header />
