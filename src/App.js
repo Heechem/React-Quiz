b5a98baf4b3504e13de552f0b4b9fa73f6e3adcd
Heechem <hichempeter@gmail.com>
2023-07-02 09:20:34 -0600

    Adding error catch

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ function reducer(state, action) {
         questions: action.payload,
         status: "ready",
       };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -30,7 +35,7 @@ const App = () => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => console.log(err));
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
